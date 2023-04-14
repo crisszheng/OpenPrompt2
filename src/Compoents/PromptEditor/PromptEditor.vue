d0c6da61d9d0a0e90c8f48831dc55b5025845f9b
crisszheng <126538946+crisszheng@users.noreply.github.com>
2023-04-14 11:26:46 +0800

    Update PromptEditor.vue

--- src/Compoents/PromptEditor/PromptEditor.vue
+++ src/Compoents/PromptEditor/PromptEditor.vue
@@ -139,7 +139,7 @@
 
         <!--    在使用 indexfs.moonvy.com 的翻译服务时显示广告，尝试给腾讯翻译的服务费回血    -->
         <div v-if="needShowAd" class="回血-box" v-tooltip="'广告商提供的内容，与本网站（Moonvy 月维）无关'">
-            <a href="https://nf.video/yinhe/web?sharedId=124758" target="_blank"> <img src="./Assets/ad.png" /> </a>
+            <a href="https://www.ohhai.top/" target="_blank"> <img src="./Assets/ad.png" /> </a>
         </div>
     </div>
 </template>
