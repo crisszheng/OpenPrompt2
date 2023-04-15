2c5c1dd4a7dade62b004061bca741f9cd7ec62a4
crisszheng <126538946+crisszheng@users.noreply.github.com>
2023-04-15 11:18:02 +0800

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,29 +1,45 @@
 // @ts-ignore
-import express from "express"
-// @ts-ignore
-import cors from "cors"
-import * as dotenv from "dotenv"
-import { translate } from "./translate"
-dotenv.config()
-const app = express()
+// 导入 express 库
+const express = require("express");
+// 导入 cors 库，用于跨域请求处理
+const cors = require("cors");
+const dotenv = require("dotenv");
+// 导入自定义的翻译函数
+const { translate } = require("./translate");
+dotenv.config();
 
-app.use(express.json())
-app.use(cors())
+// 创建一个 express 应用实例
+const app = express();
 
-app.post("/prompt-studio/translate/prompts", async (req: any, res: any) => {
-    let input: { words: string[]; to: string } = req.body
-    let orgText = input.words.join("\n")
-    const finText = await translate({ text: orgText, to: input.to ?? "zh-cn", server: "tencent" })
+// 使用 JSON 解析中间件
+app.use(express.json());
+// 使用跨域中间件处理跨域请求
+app.use(cors());
 
-    if (finText) {
-        let words = finText.split("\n")
-        res.json(words)
-    } else {
-        res.json([])
-    }
-})
+// 处理 POST 请求 /translate
+app.post("/translate", async (req, res) => {
+  // 获取请求的 body 中的参数
+  let input = req.body;
+  // 将参数数组拼接成字符串
+  let orgText = input.words.join("\n");
+  // 调用自定义的翻译函数
+  const finText = await translate({
+    text: orgText,
+    to: input.to ?? "zh-cn",
+    server: "tencent",
+  });
 
-/* const port = process.env.PORT || 19212 */
+  // 根据翻译结果响应返回给客户端
+  if (finText) {
+    let words = finText.split("\n");
+    res.json(words);
+  } else {
+    res.json([]);
+  }
+});
+
+// 启动应用，监听指定端口
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Server started on port ${port}`)
-})
+  console.log(`Server started on port ${port}`);
+});
